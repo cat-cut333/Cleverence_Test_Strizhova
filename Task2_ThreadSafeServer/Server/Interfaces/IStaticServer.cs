eb1d7e74a6df2c774b6c59f5da0820ebee0b9d1c
Strih <katia.strizhova02@yandex.ru>
2026-08-19 21:10:39 +0300

    feat: add IStaticServer interface

---
+++ Task2_ThreadSafeServer/Server/Interfaces/IStaticServer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_ThreadSafeServer.Server.Interfaces
+{
+    /// <summary>
+    /// Defines a contract for a thread-safe static server.
+    /// </summary>
+    public interface IStaticServer
+    {
+        /// <summary>
+        /// Gets the current count in a thread-safe manner.
+        /// </summary>
+        int GetCount();
+
+        /// <summary>
+        /// Adds a value to the count in a thread-safe manner.
+        /// </summary>
+        void AddToCount(int value);
+    }
+}
