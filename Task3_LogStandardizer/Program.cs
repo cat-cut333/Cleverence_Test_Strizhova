a0cd5f1982989d8e6505b71c613969e67ea69960
Strih <katia.strizhova02@yandex.ru>
2026-08-19 23:08:36 +0300

    chore: add project skeleton for Task3

---
+++ Task3_LogStandardizer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task3_LogStandardizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
