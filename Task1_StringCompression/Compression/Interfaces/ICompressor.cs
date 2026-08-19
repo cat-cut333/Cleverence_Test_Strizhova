e86aa1e0896a98de947c93c23ee36cf31edb18d7
Strih <katia.strizhova02@yandex.ru>
2026-08-19 17:17:13 +0300

    feat: add ICompressor and IValidator interfaces

---
+++ Task1_StringCompression/Compression/Interfaces/ICompressor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_StringCompression.Compression.Interfaces
+{
+    public interface ICompressor
+    {
+        string Compress(string input);
+        string Decompress(string input);
+    }
+}
---
+++ Task1_StringCompression/Validation/Interfaces/IValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_StringCompression.Validation.Interfaces
+{
+    interface IValidator<T>
+    {
+        bool IsValid(T input, bool allowEmpty = false);
+        void ValidateOrThrow(T input, bool allowEmpty = false);
+    }
+}
