ffe71e2d688b9b7e68b2865a03efa28da238e9d6
Strih <katia.strizhova02@yandex.ru>
2026-08-19 17:14:27 +0300

    chore: add project skeleton for Task1

---
+++ Task1_StringCompression/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task1_StringCompression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
