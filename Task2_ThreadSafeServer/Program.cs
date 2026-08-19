75a9e20a1383799e4677d3859ba34ccd9025a13a
Strih <katia.strizhova02@yandex.ru>
2026-08-19 21:08:21 +0300

    chore: add project skeleton for Task2

---
+++ Task2_ThreadSafeServer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task2_ThreadSafeServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
